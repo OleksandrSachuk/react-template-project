cce4139e2042f07815912c35e95af82a55f05e06
Oleksandr Sachuk <ovsachuk@gmail.com>
2018-12-05 17:17:19 +0200

    Add scss support

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -349,6 +349,7 @@ module.exports = {
             exclude: cssModuleRegex,
             loader: getStyleLoaders({
               importLoaders: 1,
+              modules: true,
               sourceMap: shouldUseSourceMap,
             }),
             // Don't consider CSS imports dead code even if the
