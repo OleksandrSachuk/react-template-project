7adadf59f0bd0f976112443083dbc24c35b8e269
Oleksandr Sachuk <ovsachuk@gmail.com>
2018-12-06 12:14:56 +0200

    Add redux-thunk

--- src/configurations/store.js
+++ src/configurations/store.js
@@ -1,4 +1,5 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
@@ -13,7 +14,7 @@ const rootEpic = combineEpics(templateEpic);
 
 export default function configureStore(preloadedState) {
   const epicMiddleware = createEpicMiddleware();
-  const middlewares = [epicMiddleware];
+  const middlewares = [thunk, epicMiddleware];
   const middlewareEnhancer = applyMiddleware(...middlewares);
   const enhancers = [middlewareEnhancer];
   const composedEnhancers = composeWithDevTools(...enhancers);
