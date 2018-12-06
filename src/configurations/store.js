8b50b57ae4e6b08796f9d58cd61c7ed3a7a2a952
Oleksandr Sachuk <ovsachuk@gmail.com>
2018-12-06 16:29:31 +0200

    Add redux-persist

--- src/configurations/store.js
+++ src/configurations/store.js
@@ -2,9 +2,18 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
+import config from './config';
+import logger from './logger';
 import template from '../modules/module1/reducers';
 import templateEpic from '../modules/module1/epics';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+const isEnableLogs = config.enable_logs;
+const isEnableDevtools = config.enable_devtools;
 
 const rootReducer = combineReducers({
   template,
@@ -12,16 +21,40 @@ const rootReducer = combineReducers({
 
 const rootEpic = combineEpics(templateEpic);
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export default function configureStore(preloadedState) {
   const epicMiddleware = createEpicMiddleware();
+
   const middlewares = [thunk, epicMiddleware];
+
+  if (isDevelopment && isEnableLogs) {
+    middlewares.push(logger);
+  }
+
   const middlewareEnhancer = applyMiddleware(...middlewares);
   const enhancers = [middlewareEnhancer];
-  const composedEnhancers = composeWithDevTools(...enhancers);
+  let composedEnhancers = null;
 
-  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+  if (isDevelopment && isEnableDevtools) {
+    composedEnhancers = composeWithDevTools(...enhancers);
+  }
+
+  const store = createStore(
+    persistedReducer,
+    preloadedState,
+    composedEnhancers || enhancers,
+  );
 
   epicMiddleware.run(rootEpic);
 
-  return store;
+  const persistor = persistStore(store);
+
+  return { store, persistor };
 }
--- src/modules/module1/epics/templateEpic.js
+++ src/modules/module1/epics/templateEpic.js
@@ -1,4 +1,7 @@
 import { ofType } from 'redux-observable';
-import { tap } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
-export const templateEpic = action$ => action$.pipe();
+export const templateEpic = action$ => action$.pipe(
+    ofType('TEST'),
+    tap(() => console.log('epic')),
+  );
