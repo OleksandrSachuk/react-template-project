a3d7af3c26854e7d2fa965b13576fe07b497d83f
Oleksandr Sachuk <ovsachuk@gmail.com>
2018-12-10 13:19:33 +0200

    Add typescript

---
+++ src/modules/module1/components/Component1/Component1Container.tsx
@@ -0,0 +1,16 @@
+import { bindActionCreators, Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+
+import Component1 from './Component1';
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = (dispatch: Dispatch<any>) => bindActionCreators({}, dispatch);
+
+export default compose(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps,
+    ),
+)(Component1);
