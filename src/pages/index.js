4caef050b3b3528cc4536cc25b820b40c455251e
Oleksandr Sachuk <ovsachuk@gmail.com>
2018-12-11 14:24:08 +0200

    Add react lazy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 
 import PrivatePage from './PrivatePage';
-import App from '../App';
 import { Component1 } from '../modules/module1/components';
 
 const propTypes = {
   history: PropTypes.object.isRequired,
 };
 
+const App = lazy(() => import('../App'));
+
 const Pages = ({ history }) => (
   <ConnectedRouter history={ history }>
     <div>
-      <Switch>
-        <Route exact path="/" component={ App } />
-        <PrivatePage path="/component1" component={ Component1 } />
-      </Switch>
+      <Suspense fallback={ <div>Loading...</div> }>
+        <Switch>
+          <Route exact path="/" component={ props => <App { ...props } /> } />
+          <PrivatePage path="/component1" component={ Component1 } />
+        </Switch>
+      </Suspense>
     </div>
   </ConnectedRouter>
 );
